2b84f35ae21b29e046dd183e88be89fd607cd819
Jin Wang <wangatqueens@gmail.com>
2020-04-21 22:40:56 -0400

    Updates

---
+++ precache-manifest.b719b4ccddbb1f0d4a4d02ea193790f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484f151ee0386e6c46c9ed5c49ac396e",
+    "url": "/un-rite/index.html"
+  },
+  {
+    "revision": "75057e93a0add9b60478",
+    "url": "/un-rite/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0fc4126c17be2fb3a0a2",
+    "url": "/un-rite/static/js/2.11bb3a16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/un-rite/static/js/2.11bb3a16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75057e93a0add9b60478",
+    "url": "/un-rite/static/js/main.ccb1372a.chunk.js"
+  },
+  {
+    "revision": "5ad3e628e93a893a7806",
+    "url": "/un-rite/static/js/runtime-main.614645a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/un-rite/static/media/logo.5d5d9eef.svg"
+  }
+]);
