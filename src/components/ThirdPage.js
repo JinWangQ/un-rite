ea770060ef6fd16a795649d402b823c8946945f3
Jin Wang <wangatqueens@gmail.com>
2020-04-21 23:52:00 -0400

    Update ThirdPage.js

--- src/components/ThirdPage.js
+++ src/components/ThirdPage.js
@@ -2,7 +2,6 @@ import React from "react";
 import Box from "@material-ui/core/Box";
 import { makeStyles } from "@material-ui/core/styles";
 import pic from "../assets/pic.jpg";
-import Button from "@material-ui/core/Button";
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
